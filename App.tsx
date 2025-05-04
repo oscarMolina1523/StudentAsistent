a4d349c42644f40e4473a4d80b1fd0140833e32e
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-05-03 19:10:34 -0600

    solution to icons with a larges charge

--- App.tsx
+++ App.tsx
@@ -1,6 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { View, ActivityIndicator } from 'react-native';
 import Navigation from './src/Navigation';
 
-export default function App() {
+export default function App(): JSX.Element {
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    const loadFonts = async (): Promise<void> => {
+      await Font.loadAsync({
+        ...Ionicons.font,
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#00ba8b" />
+      </View>
+    );
+  }
+
   return <Navigation />;
 }
-
